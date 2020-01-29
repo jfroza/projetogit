361e766f9422c9e48dab63e10f7c6523785b8436
jfroza <jaderfroza@gmail.com>
2020-01-29 13:00:28 -0300

    Add files via upload

--- src/Router.js
+++ src/Router.js
@@ -1,1 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Inicial from "./pages/inicial/Inicial";
+import Confirmacao from "./pages/confirmacao/confirmacao";
+import SaibaMais from "./pages/saiba-mais/SaibaMais";
 
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Inicial} />
+      <Route exact path="/confirmacao" component={Confirmacao} />
+      <Route exact path="/saiba-mais" component={SaibaMais} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
